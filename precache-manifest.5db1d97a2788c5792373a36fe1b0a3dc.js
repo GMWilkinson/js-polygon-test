886f1298898211c28d9498e23fd730ed6b72b83c
Grant Wilkinson <grant@aimermedia.com>
2020-07-09 19:54:57 +0100

    Updates

---
+++ precache-manifest.5db1d97a2788c5792373a36fe1b0a3dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2f95f00522e0f433f13561d9d96ec75",
+    "url": "/js-polygon-test/index.html"
+  },
+  {
+    "revision": "9c169b63443228323195",
+    "url": "/js-polygon-test/static/css/main.a4de48b0.chunk.css"
+  },
+  {
+    "revision": "18258c87cfecabdccbd7",
+    "url": "/js-polygon-test/static/js/2.a34b0282.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/js-polygon-test/static/js/2.a34b0282.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c169b63443228323195",
+    "url": "/js-polygon-test/static/js/main.f461e16a.chunk.js"
+  },
+  {
+    "revision": "7c6a096687caa94109d3",
+    "url": "/js-polygon-test/static/js/runtime-main.e6e6a60f.js"
+  }
+]);
